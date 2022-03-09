f659a429d8d8de08323f3be99189ce56c1ed67c3
Olugbenga Raymond <olugbengaraymond20@gmail.com>
2022-03-09 12:44:45 +0100

    creating the stake page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import Home from './Home';
+import React from 'react';
+// import { Routes, Route} from "react-router-dom";
+// import Home from './Home';
+import Stake from './Stake';
 import './App.css';
 import './Style.css';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Stake />
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="stake" element={<Stake />} />
+      </Routes> */}
     </div>
   );
 }
--- src/Stake.js
+++ src/Stake.js
@@ -5,6 +5,39 @@ function Stake() {
     return (
         <div className='Stake'>
             <Header />
+            <main>
+                <h1>
+                    Total HEAP Staked <br/>
+                    &#36;1,500,000,000
+                </h1>
+                <div className='StakeTable'>
+                    <div>
+                        <strong>Manual HEAP</strong> <br/>
+                        <span>Earn HEAP, Stake HEAP</span>
+                    </div>
+                    <div>
+                        <strong>HEAP Earned</strong><br/>
+                        0.0 <br/>
+                        0 USB
+                    </div>
+                    <div>
+                        <strong>HEAP Staked</strong> <br/>
+                        0.0 <br/>
+                        0 USB
+                    </div>
+                    <div>
+                        APR <br/>
+                        <b>46.96%</b>
+                    </div>
+                    <div>
+                        Total Staked <br/>
+                        <b>46.96228</b>
+                    </div>
+                    <div>
+                        Details
+                    </div>
+                </div>
+            </main>
         </div>
     );
 }
